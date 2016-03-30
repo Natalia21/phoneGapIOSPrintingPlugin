437bc466fab7b4886ad0d09d62490742b34dd235
Dmitry Petrovets <petrovets@intspirit.com>
2016-03-30 19:23:08 +0300

    fixes

--- src/ios/Nprint.m
+++ src/ios/Nprint.m
@@ -42,7 +42,7 @@
 - (void) print:(CDVInvokedUrlCommand*)command
 {
     int find_printers_status = EPSONIO_OC_SUCCESS;
-    find_printers_status = [EpsonIoFinder start:EPSONIO_OC_DEVTYPE_BLUETOOTH];
+    find_printers_status = [EpsonIoFinder start:EPSONIO_OC_DEVTYPE_BLUETOOTH FindOption:nil];
 
     int get_printers_list_status = EPSONIO_OC_SUCCESS;
     if ( find_printers_status == EPSONIO_OC_SUCCESS) {
@@ -50,58 +50,60 @@
         [EpsonIoFinder getDeviceInfoList:&get_printers_list_status
         FilterOption:EPSONIO_OC_PARAM_DEFAULT]];
         [EpsonIoFinder stop];
+    
+
+        if ( [&printerList_ count] < 1 ) {
+            return;
+        }
+
+        NSString deviceName = [[&printerList_ objectAtIndex:0] deviceName];
+        NSString printerName = [[&printerList_ objectAtIndex:0] printerName];
+
+        //Initialize an EposBuilder class instance
+        id builder = [[EposBuilder alloc] initWithPrinterModel: printerName Lang: EPOS_OC_MODEL_ANK];
+        if ( builder == nil ) {
+            return;
+        }
+
+        NSArray * arguments = [command arguments];
+        NSString *str = [arguments objectAtIndex:0];
+
+        //Create a print document
+        int errorStatus = EPOS_OC_SUCCESS;
+        errorStatus = [builder addTextLang: EPOS_OC_LANG_EN];
+        errorStatus = [builder addTextSmooth: EPOS_OC_TRUE];
+        errorStatus = [builder addTextFont: EPOS_OC_FONT_A];
+        errorStatus = [builder addTextSize: 3 Height: 3];
+        errorStatus = [builder addText: str];
+        errorStatus = [builder addCut: EPOS_OC_CUT_FEED];
+
+        //Initialize an EposPrint class instance
+        id printer = [[EposPrint alloc] init];
+        long status;
+
+        //Send a print document
+        if (printer == nil) {
+            return;
+        }
+
+        //<Start communication with the printer>
+        errorStatus = [printer openPrinter:EPSONIO_OC_DEVTYPE_BLUETOOTH
+        DeviceName:deviceName Enabled:EPOS_OC_TRUE
+        Interval:EPOS_OC_PARAM_DEFAULT Timeout:EPOS_OC_PARAM_DEFAULT];
+
+        //<Send data>
+        errorStatus = [printer sendData:builder Timeout:10000 Status:&status];
+
+        //<Delete the command buffers>
+        if ((status & EPOS_OC_ST_PRINT_SUCCESS) == EPOS_OC_ST_PRINT_SUCCESS) {
+            errorStatus = [builder clearCommandBuffer];
+        }
+
+        //<End communication with the printer>
+        errorStatus = [printer closePrinter];
+        [printer release];
+        [builder release];
     }
-
-    if ( [&printerList_ count] < 1 ) {
-        return;
-    }
-
-    NSString deviceName = [[&printerList_ objectAtIndex:0] deviceName];
-    NSString printerName = [[&printerList_ objectAtIndex:0] printerName];
-
-    //Initialize an EposBuilder class instance
-    id builder = [[EposBuilder alloc] initWithPrinterModel: printerName Lang: EPOS_OC_MODEL_ANK];
-    if ( builder == nil ) {
-        return;
-    }
-
-    NSString *str = [arguments objectAtIndex:0];
-
-    //Create a print document
-    int errorStatus = EPOS_OC_SUCCESS;
-    errorStatus = [builder addTextLang: EPOS_OC_LANG_EN];
-    errorStatus = [builder addTextSmooth: EPOS_OC_TRUE];
-    errorStatus = [builder addTextFont: EPOS_OC_FONT_A];
-    errorStatus = [builder addTextSize: 3 Height: 3];
-    errorStatus = [builder addText: str];
-    errorStatus = [builder addCut: EPOS_OC_CUT_FEED];
-
-    //Initialize an EposPrint class instance
-    id printer = [[EposPrint alloc] init];
-    long status;
-
-    //Send a print document
-    if (printer == nil) {
-        return;
-    }
-
-    //<Start communication with the printer>
-    errorStatus = [printer openPrinter:EPSONIO_OC_DEVTYPE_BLUETOOTH
-    DeviceName:deviceName Enabled:EPOS_OC_TRUE
-    Interval:EPOS_OC_PARAM_DEFAULT Timeout:EPOS_OC_PARAM_DEFAULT];
-
-    //<Send data>
-    errorStatus = [printer sendData:builder Timeout:10000 Status:&status];
-
-    //<Delete the command buffers>
-    if ((status & EPOS_OC_ST_PRINT_SUCCESS) == EPOS_OC_ST_PRINT_SUCCESS) {
-        errorStatus = [builder clearCommandBuffer];
-    }
-
-    //<End communication with the printer>
-    errorStatus = [printer closePrinter];
-    [printer release];
-    [builder release];
 }
 
 @end
